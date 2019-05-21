feaa7adbcb8064fa26976b7677db3085d7dd1a58
Christian Heimes <christian@python.org>
2019-05-21 11:13:13 +0200

    Add setup.py for NIS
    Signed-off-by: Christian Heimes <christian@python.org>

---
+++ nis/setup.py
@@ -0,0 +1,62 @@
+import os
+import shlex
+import sysconfig
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+class NISBuildExt(build_ext):
+    def build_extensions(self):
+        libraries = []
+        includes_dirs = []
+        library_dirs = []
+
+        lib_search_dirs = self.compiler.library_dirs + [
+            "/lib64",
+            "/usr/lib64",
+            "/lib",
+            "/usr/lib",
+        ]
+
+        if self.compiler.find_library_file(lib_search_dirs, "nsl"):
+            libraries.append("nsl")
+        else:
+            nsl_dirs = [
+                os.path.join(lib_dir, "nsl") for lib_dir in lib_search_dirs
+            ]
+            libnsl = self.compiler.find_library_file(nsl_dirs, "nsl")
+            if libnsl is not None:
+                library_dirs.append(os.path.dirname(libnsl))
+                libraries.append("nsl")
+
+        if self.compiler.find_library_file(lib_search_dirs, "tirpc"):
+            libraries.append("tirpc")
+
+        for inc_dir in self.compiler.include_dirs + ["/usr/include"]:
+            nsl_dir = os.path.join(inc_dir, "nsl")
+            if os.path.isfile(os.path.join(nsl_dir, "rpcsvc/yp_prot.h")):
+                includes_dirs.append(nsl_dir)
+            tirpc_dir = os.path.join(inc_dir, "tirpc")
+            if os.path.isfile(os.path.join(tirpc_dir, "rpc/rpc.h")):
+                includes_dirs.append(tirpc_dir)
+
+        for ext in self.extensions:
+            ext.libraries[:] = libraries
+            ext.library_dirs[:] = library_dirs
+            ext.include_dirs[:] = includes_dirs
+
+        super().build_extensions()
+
+
+setup(
+    name="nis",
+    version="3.7.3",
+    author="CPython",
+    author_email="python-dev@python.org",
+    url="https://www.python.org/",
+    license="PSFL",
+    package_dir={"": "src"},
+    cmdclass={"build_ext": NISBuildExt},
+    ext_modules=[Extension("nis", sources=["src/nismodule.c"])],
+)
